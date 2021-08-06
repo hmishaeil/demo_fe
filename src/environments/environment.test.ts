9c80e1b2cc022b44d95eabf9c202f9226e542f00
group 28 <hmishaeil@gmail.com>
2021-08-06 15:35:40 -0700

    changes

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  API_URL: 'http://ec2-100-25-102-167.compute-1.amazonaws.com:8080',
+  API_URL: 'http://ec2-100-25-102-167.compute-1.amazonaws.com:8080/demo-0.0.1-SNAPSHOT',
 };
